7c90c692e73bc2721dde46dcd22555c5722bcf70
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-07 22:54:09 +1000

    added InvalidQuantity exception

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="symbol_cache",
-    version="0.24",
+    version="0.25",
     description="Stock ohlc data caching",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
