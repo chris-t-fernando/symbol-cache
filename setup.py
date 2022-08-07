752a5d17f9e4e597b22f20b7f7e6288b86569a76
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-08 08:35:18 +1000

    added invalidprice exception

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="symbol_cache",
-    version="0.25",
+    version="0.26",
     description="Stock ohlc data caching",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
