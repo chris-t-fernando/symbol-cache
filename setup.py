e4950f1b25b977cfccda8785fec18fea54579b5b
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-14 00:20:44 +1000

    bad paste

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="symbol_cache",
-    version="0.28",
+    version="0.29",
     description="Stock ohlc data caching",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
