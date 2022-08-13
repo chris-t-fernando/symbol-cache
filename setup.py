ea996058dcd4d8feda2dcb05171c015f52c9f782
Chris Fernando <chris.t.fernando@gmail.com>
2022-08-13 21:00:14 +1000

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="symbol_cache",
-    version="0.27",
+    version="0.28",
     description="Stock ohlc data caching",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
