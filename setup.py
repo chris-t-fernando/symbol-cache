d27f345cfa079cf3fed107c1e7ce473e01039b66
Chris Fernando <chris.t.fernando@gmail.com>
2023-03-26 18:31:28 +1100

    bug in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name="symbol_cache",
-    version="0.43",
+    version="0.44",
     description="Stock ohlc data caching",
     author="Chris Fernando",
     author_email="chris.t.fernando@gmail.com",
     url="https://github.com/chris-t-fernando/symbol-cache",
-    packages=["symbol"],
+    packages=["symbol_cache"],
     install_requires=["numpy", "pandas", "yfinance"],
 )
